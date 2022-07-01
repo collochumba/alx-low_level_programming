09b01c21c63029e9bc145c4bd44c73e7016fc918
collochumba <chumbasea@gmail.com>
2022-06-30 17:21:49 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main-prints exactly "and that piece of act is usefull"
+ * main-prints exactly "and that piece of art is usefull"
  * - Collins Chumba, 2022-07-01",
  * followed by a newline, to the standard error.
  * Return: i if succes.
